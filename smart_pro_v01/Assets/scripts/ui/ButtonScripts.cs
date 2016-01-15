e1b48830e7c7d6113f9e1e2c204e03c19103cdf6
worrywang <ff_worry@hotmail.com>
2016-01-15 14:46:37 +0800

    first project add

---
+++ smart_pro_v01/Assets/scripts/ui/ButtonScripts.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnLabelButtonClick() {
+        print("Label button be click.");
+    }
+
+    public void OnSpriteButtonClick() {
+        print("SpriteButton has be clicked.");
+    }
+
+    public void DisabledButton(GameObject go) {
+        if (go != null) {
+            UIButton[] ub = go.GetComponents<UIButton>();
+            Debug.Log("disabled button is pressed.");
+
+            if (ub != null && ub.Length > 0)
+            {
+                Debug.Log("UIButton list sizes "+ub.Length);
+                //for (int i = 0; i < ub.Length; i++)
+                //{
+                //    if (ub[i].isEnabled)
+                //    {
+                //        ub[i].enabled = false;
+                //    }
+                //    else
+                //    {
+                //        ub[i].enabled = true;
+                //    }
+                //}
+
+            }
+        }
+    }
+}
